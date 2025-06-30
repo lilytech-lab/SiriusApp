4dd1c69b530cfe7d26979fe077f670bd88d1dcd3
Masami Sugao <15377854+masami-sugao@users.noreply.github.com>
2025-06-30 17:02:55 +0900

    :recycle: LiteMidiPatchbayモードをMidiForwardingモードに名称変更

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -81,8 +81,8 @@
       "url": "_framework\/MudBlazor.xjdb9om7fa.wasm.br"
     },
     {
-      "hash": "sha256-jFGasSCL2DGNDkkYVwr4FYEx5C2Eg1V8NE4J0sAwQAc=",
-      "url": "_framework\/SiriusApp.mz2nlhb52e.wasm.br"
+      "hash": "sha256-hnVT8ugXmEnXstYL1DStg6MeeyF73UK1WVPnI4rLUdA=",
+      "url": "_framework\/SiriusApp.cc3ys22ie6.wasm.br"
     },
     {
       "hash": "sha256-JI5zz8ELp6GMT6g7f0N0rRr5RHx7\/IulJwTJViWJUPA=",
@@ -185,7 +185,7 @@
       "url": "_framework\/System.ezrw3d5kw2.wasm.br"
     },
     {
-      "hash": "sha256-NWCaOMa7LvCv6uCf6WDwVghoRdltSDbPWoqfjwIusI4=",
+      "hash": "sha256-l+ysdK4GSEme+GAEu5X5pqIqzC+bo6BmeBqZ0TBmtgg=",
       "url": "_framework\/blazor.boot.json.br"
     },
     {
@@ -433,5 +433,5 @@
       "url": "brotliloader.min.js"
     }
   ],
-  "version": "eyyGwkFS"
+  "version": "GRy7LkAY"
 };
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: eyyGwkFS */
+/* Manifest version: GRy7LkAY */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
